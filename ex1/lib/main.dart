de24a331fa45db84b2defbec8ce64d24790d9b70
Kiwon Hong <superbox@dtime.co>
2022-03-21 16:38:11 +0900

    비만도 계산기 예제 - 결과 화면 작성

--- ex1/lib/main.dart
+++ ex1/lib/main.dart
@@ -83,10 +83,34 @@ class _BmiMainState extends State<BmiMain> {
 }
 
 class BmiResult extends StatelessWidget {
-  const BmiResult({Key? key}) : super(key: key);
+  final double height;
+  final double weight;
+
+  BmiResult(this.height, this.weight);  // 키와 몸무게를 받는 생성자
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text('비만도 계산기'),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '정상',
+              style: TextStyle(fontSize: 36),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Icon( //
+              Icons.sentiment_satisfied,
+              color: Colors.green,
+              size: 100,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
