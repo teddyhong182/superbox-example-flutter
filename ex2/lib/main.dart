efc0d24d87b1856447d73bf9bed8ee1da773e322
Kiwon Hong <superbox@dtime.co>
2022-03-21 17:36:17 +0900

    스톱워치 - 타이머 기능 구현

--- ex2/lib/main.dart
+++ ex2/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -37,6 +39,20 @@ class StopWatchPage extends StatefulWidget {
 }
 
 class _StopWatchPageState extends State<StopWatchPage> {
+  late Timer _timer; // 타이머
+
+  var _time = 0;  // 0.01초마다 1씩 증가시킬 정수형 변수
+  var _isRunning = false; // 현재 시작 상태를 나타낼 불리언 변수
+
+  // 랩타임에 표시할 시간을 저장할 리스트
+  List<String> _lapTimes = [];
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,13 +69,17 @@ class _StopWatchPageState extends State<StopWatchPage> {
         onPressed: () => setState(() {
           _clickButton();
         }),
-        child: Icon(Icons.play_arrow),
+        child: _isRunning ? Icon(Icons.pause) : Icon(Icons.play_arrow),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 
   Widget _buildBody() {
+    var sec = _time ~/ 100;
+    // 1/100초
+    var hundredth = '${_time % 100}'.padLeft(2, '0');
+
     return Center(
       child: Padding(
         padding: const EdgeInsets.only(top: 30),
@@ -72,10 +92,10 @@ class _StopWatchPageState extends State<StopWatchPage> {
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
                     Text(
-                      '0',
+                      '$sec',
                       style: TextStyle(fontSize: 50.0),
                     ),
-                    Text('00'),
+                    Text('$hundredth'),
                   ],
                 ),
                 Container(
@@ -92,7 +112,7 @@ class _StopWatchPageState extends State<StopWatchPage> {
               bottom: 10,
               child: FloatingActionButton(
                 backgroundColor: Colors.deepOrange,
-                onPressed: () {},
+                onPressed: _reset,
                 child: Icon(Icons.rotate_left),
               ),
             ),
@@ -111,9 +131,36 @@ class _StopWatchPageState extends State<StopWatchPage> {
     );
   }
   // 시작 또는 일시정지 버튼 클릭
-  void _clickButton() {}
+  void _clickButton() {
+    _isRunning = !_isRunning;
 
+    if (_isRunning) {
+      _start();
+    } else {
+      _pause();
+    }
+  }
 
+  void _start() {
+    _timer = Timer.periodic(Duration(milliseconds: 10), (timer) {
+      setState(() {
+        _time++;
+      });
+    });
+  }
+
+  void _pause() {
+    _timer?.cancel();
+  }
+
+  void _reset() {
+    setState(() {
+      _isRunning = false;
+      _timer?.cancel();
+      _lapTimes.clear();
+      _time = 0;
+    });
+  }
 }
 
 
